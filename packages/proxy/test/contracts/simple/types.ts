a34dc0989ce895deaab21b074bbd485335db8c57
Hank Stoever <hstove@gmail.com>
2021-04-06 17:04:24 -0700

    feat: remove proxy package

--- packages/proxy/test/contracts/simple/types.ts
+++
@@ -1,6 +0,0 @@
-import { ClarityTypes, Transaction } from '@clarion/proxy';
-
-export interface SimpleContract {
-  getName: () => Transaction<ClarityTypes.StringAsciiCV, ClarityTypes.NoneCV>;
-  getNumber: () => Promise<ClarityTypes.UIntCV>;
-}
